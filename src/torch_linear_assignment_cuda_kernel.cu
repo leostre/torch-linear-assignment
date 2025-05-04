16447eecd67351099a193a179af42f1d8a20df0d
leostre <strelllleo@mail.ru>
2025-05-04 12:07:35 +0000

    parallel crop

--- src/torch_linear_assignment_cuda_kernel.cu
+++ src/torch_linear_assignment_cuda_kernel.cu
@@ -181,7 +181,7 @@ __device__
        }
      }
  
-     for (int j = 0; j < nc; j++) {
+     for (int j = 0; j < limit; j++) {
        if (SC[j]) {
          v[j] -= minVal - shortestPathCosts[j];
        }
