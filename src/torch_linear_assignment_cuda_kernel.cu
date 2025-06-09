ee9157bd4ee5e65df7379315d83167f484015a2d
leostre <strelllleo@mail.ru>
2025-06-09 09:09:56 +0000

    main change

--- src/torch_linear_assignment_cuda_kernel.cu
+++ src/torch_linear_assignment_cuda_kernel.cu
@@ -18,35 +18,7 @@
 
 typedef unsigned char uint8_t;
 
-
-int SMPCores(int device_index)
-{
-  cudaDeviceProp devProp;
-  cudaGetDeviceProperties(&devProp, device_index);
-  switch (devProp.major){
-  case 2: // Fermi
-    if (devProp.minor == 1)
-      return 48;
-    else return 32;
-  case 3: // Kepler
-    return 192;
-  case 5: // Maxwell
-    return 128;
-  case 6: // Pascal
-    if ((devProp.minor == 1) || (devProp.minor == 2)) return 128;
-    else if (devProp.minor == 0) return 64;
-  case 7: // Volta and Turing
-    if ((devProp.minor == 0) || (devProp.minor == 5)) return 64;
-  case 8: // Ampere
-    if (devProp.minor == 0) return 64;
-    else if (devProp.minor == 6) return 128;
-    else if (devProp.minor == 9) return 128; // ada lovelace
-  case 9: // Hopper
-    if (devProp.minor == 0) return 128;
-  // Unknown device;
-  }
-  return 128;
-}
+const int WARP_SIZE = 16;
 
 
 template <typename scalar_t>
@@ -230,7 +202,7 @@ void solve_cuda_batch(c10::ScalarType scalar_type,
   torch::Tensor SC = torch::empty({bs * nc}, uint8_opt);
   torch::Tensor remaining = torch::empty({bs * nc}, int_opt);
 
-  static const int blockSize = SMPCores(device_index);
+  static const int blockSize = WARP_SIZE; //SMPCores(device_index);
   int gridSize = (bs + blockSize - 1) / blockSize;
   at::cuda::CUDAStream stream = at::cuda::getCurrentCUDAStream(device_index);
   solve_cuda_kernel_batch<<<gridSize, blockSize, 0, stream.stream()>>>(
