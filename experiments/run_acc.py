3c89bbd95eaa5765943cc76735b5e36a772c8ac6
leostre <strelllleo@mail.ru>
2025-06-15 11:49:07 +0000

    upd exp

--- experiments/run_acc.py
+++ experiments/run_acc.py
@@ -29,59 +29,56 @@ _TYPES = {
     'f16': torch.float16,
     'f32': torch.float32
 }
+ACC = True
 
-real_costs = torch.load('/root/torch-linear-assignment/data/train-end.pth')
+nc = nr = 8
 
-def nr_nc(bs, type):
+def nr_nc(bs, type, acc=False):
+    total = bs * nc * nr
+    costs = torch.arange(total, dtype=torch.float)[torch.randperm(total)].reshape(bs, nr, nc).contiguous()
     if is_cuda:
-        costs = real_costs[:bs].to('cuda').to(_TYPES[type])
+        costs = costs.to('cuda').to(_TYPES[type])
     assert costs.dtype == _TYPES[type]
     try:
         torch.cuda.synchronize()
         t = time()
-        batch_linear_assignment(costs)
+        result = batch_linear_assignment(costs)[0]
         t = time() - t
+        acc = 1
+        if acc:
+            result_ref= batch_linear_assignment(costs.to('cpu'))[0]
+        acc = (result == result_ref) / len(result)       
+
     except:
-        return -1e-5
+        return -1e-5, acc
     finally:
         del costs 
-        torch.cuda.empty_cache()
-    return t
+        # torch.cuda.empty_cache()
+    return t, acc
 
 print('STARTED BS EVALUATION'.center(80, '+'))
 for TYPE in _TYPES:
     results = []
-    REPS = 20
+    REPS = 10
     bss = [4, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16364]
 
     for bs in tqdm(bss):
         times = [] 
+        accs = []
         for i in range(REPS):
-            t = nr_nc(bs, TYPE)
+            t, acc = nr_nc(bs, TYPE)
             times.append(t)
+            accs.append(acc)
         times = np.array(times)
+        accs = np.array(accs)
         results.append(
-            (bs, times.mean(), times.std())
+            (bs, times.mean(), times.std(), accs.mean(), accs.std())
         )
 
-    columns = ['bs', 'time_mean', 'time_std']
+    columns = ['bs', 'time_mean', 'time_std', 'accs_mean', 'accs_std']
     resdf = pd.DataFrame(data=results, columns=columns)
 
-    resdf.to_csv(f'experiments/results/bs_{EXP_NAME}_{TYPE}.csv')
+    resdf.to_csv(f'experiments/results/bs_acc_{EXP_NAME}_{TYPE}.csv')
 
 
-def nr_nc(bs, type, acc=False):
-    if is_cuda:
-        costs = real_costs[:bs].to('cuda').to(_TYPES[type])
-    assert costs.dtype == _TYPES[type]
-    try:
-        torch.cuda.synchronize()
-        t = time()
-        batch_linear_assignment(costs)
-        t = time() - t
-    except:
-        return -1e-5
-    finally:
-        del costs 
-        torch.cuda.empty_cache()
-    return t
+
